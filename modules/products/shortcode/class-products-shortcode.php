85e91cc4d0fe38fb632be69de8f56f7fda4a405f
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2019-04-02 16:48:31 +0200

    Add Shortcode

---
+++ modules/products/shortcode/class-products-shortcode.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Gestion des shortcodes des produits.
+ *
+ * @author    Eoxia <dev@eoxia.com>
+ * @copyright (c) 2011-2018 Eoxia <dev@eoxia.com>.
+ *
+ * @license   AGPLv3 <https://spdx.org/licenses/AGPL-3.0-or-later.html>
+ *
+ * @package   WPshop\Classes
+ *
+ * @since     2.0.0
+ */
+
+namespace wpshop;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Product Action Class.
+ */
+class Product_Shortcode {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 2.0.0
+	 */
+	public function __construct() {
+		add_shortcode( 'wps_product', array( $this, 'do_shortcode_product' ) );
+	}
+
+	public function do_shortcode_product( $atts ) {
+		$a = shortcode_atts( array(
+			'id'  => 0,
+			'ids' => array(),
+			'categories' => array(),
+		), $atts );
+
+		$products = array();
+		$args     = array();
+
+		if ( ! empty( $a['id'] ) ) {
+			$args['id'] = $a['id'];
+		}
+
+		if ( ! empty( $a['ids'] ) ) {
+			$a['ids']         = explode( ',', $a['ids'] );
+			$args['post__in'] = $a['ids'];
+		}
+
+		if ( ! empty( $a['categories'] ) ) {
+			$args['category_name'] = $a['categories'];
+		}
+
+		$products = Product::g()->get( $args );
+	}
+}
+
+new Product_Shortcode();
