beb8f6e7c14eff7d778df21b8d4e87801009a5ac
eoxia-amandine <amandine@eoxia.com>
2019-04-02 15:11:21 +0200

    Page taxonomy

--- modules/products/view/frontend/single-wps-product.php
+++ modules/products/view/frontend/single-wps-product.php
@@ -23,16 +23,18 @@ defined( 'ABSPATH' ) || exit; ?>
 			<?php $product = Product::g()->get( array( 'id' => get_the_ID() ), true ); ?>
 
 			<div class="wpeo-gridlayout grid-3 grid-gap-3">
-				<figure class="wps-thumbnail">
-					<?php the_post_thumbnail( 'large' ); ?>
-				</figure>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<figure class="wps-product-thumbnail">
+						<?php the_post_thumbnail( 'large' ); ?>
+					</figure>
+				<?php endif; ?>
 				<div class="wps-product-content gridw-2">
 					<header class="primary-header site-width">
 						<?php the_title( '<h1 class="page-title wps-product-title">', '</h1>' ); ?>
 					</header><!-- .primary-header -->
 
 					<div class="primary-content">
-						<div class="wps-product-price"><?php echo ! empty( $product->data['price'] ) ? esc_html( $product->data['price_ttc'] ) . '€ TTC' : ''; ?></div>
+						<div class="wps-product-price"><?php echo ! empty( $product->data['price'] ) ? esc_html( number_format( $product->data['price_ttc'], 2, ',', '' ) ) . ' €' : ''; ?></div>
 						<div class="wps-product-description"><?php the_content(); ?></div>
 						<div class="wps-product-buy wpeo-button action-attribute"
 							data-action="add_to_cart"
--- modules/products/view/frontend/taxonomy-wps-product-cat.php
+++ modules/products/view/frontend/taxonomy-wps-product-cat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Single Product view.
+ * Taxonomy Product view.
  *
  * @author    Eoxia <dev@eoxia.com>
  * @copyright (c) 2006-2018 Eoxia <dev@eoxia.com>
@@ -11,21 +11,50 @@
 
 namespace wpshop;
 
-defined( 'ABSPATH' ) || exit; ?>
+defined( 'ABSPATH' ) || exit;
 
-<?php get_header(); ?>
+get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<main id="primary" class="content-area" role="main">
 
 		<?php
-		while ( have_posts() ) :
-			echo 'bonjour';
-		endwhile; // End of the loop.
+		if ( have_posts() ) : ?>
+
+			<header class="primary-header">
+				<h1 class="page-title"><?php echo esc_html( single_cat_title() ); ?></h1>
+				<?php the_archive_description( '<div class="archive-description">', '</div>' ); ?>
+			</header><!-- .primary-header -->
+
+			<div class="wps-list-product wpeo-gridlayout grid-4">
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php $product = Product::g()->get( array( 'id' => get_the_ID() ), true ); ?>
+
+					<div class="wps-product">
+						<figure class="wps-product-thumbnail">
+							<?php the_post_thumbnail( 'large' ); ?>
+						</figure>
+						<div class="wps-product-content">
+							<div class="wps-product-title"><?php the_title(); ?></div>
+							<div class="wpeo-gridlayout grid-2">
+								<div class="wps-product-price"><?php echo ! empty( $product->data['price'] ) ? esc_html( number_format( $product->data['price_ttc'], 2, ',', '' ) ) . ' €' : ''; ?></div>
+								<div class="wps-product-buy wpeo-button button-square-40 action-attribute"
+									data-action="add_to_cart"
+									data-nonce="<?php echo wp_create_nonce( 'add_to_cart' ); ?>"
+									data-id="<?php echo esc_attr( the_ID() ); ?>"><i class="button-icon fas fa-cart-arrow-down"></i></div>
+							</div>
+						</div>
+					</div>
+				<?php endwhile; ?>
+			</div>
+
+			<?php beflex_pagination();
+
+		endif;
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
