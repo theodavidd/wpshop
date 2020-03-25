43fc3a204586a119e27c0fc5c2be4b84a54f2038
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2020-03-25 09:25:36 +0100

    Handle more strongly sync or associate system

--- modules/doli-sync/view/sync-item.view.php
+++ modules/doli-sync/view/sync-item.view.php
@@ -14,30 +14,21 @@
 
 namespace wpshop;
 
+/**
+ * @var mixed $object Object can be Product/Propal/Third_Party.
+ */
+
 defined( 'ABSPATH' ) || exit; ?>
 
-<div class="table-cell table-100 wps-sync" data-id="<?php echo $object->data['id']; ?>" data-associate="<?php echo ! empty( $object->data['external_id'] ) ? "true" : "false"; ?>">
-	<?php
-	if ( 'view' === $mode ) :
-		?>
-		<div class="button-synchro <?php echo ! empty( $object->data['external_id'] ) ? 'action-attribute' : 'wpeo-modal-event'; ?>"
-			data-class="synchro-single"
-			<?php // translators: Associate and synchronize object name. ?>
-			data-title="<?php printf( __( 'Associate and synchronize %s', 'wpshop' ), $object->data['title'] ); ?>"
-			data-action="<?php echo ! empty( $object->data['external_id'] ) ? 'sync_entry' : 'load_associate_modal'; ?>"
-			data-nonce="<?php echo esc_attr( wp_create_nonce( ! empty( $object->data['external_id'] ) ? 'sync_entry' : 'load_associate_modal' ) ); ?>"
-			data-type="<?php echo esc_attr( $doli_class ); ?>"
-			data-wp-type="<?php echo esc_attr( $wp_class ); ?>"
-			data-entry-id="<?php echo $object->data['external_id']; ?>"
-			data-wp-id="<?php echo $object->data['id']; ?>"
-			data-route="<?php echo esc_attr( $route ); ?>"
-			data-from="dolibarr"><i class="fas fa-sync"></i></div>
-		<?php
-	else :
-		?>
-		<div class="wpeo-button button-synchro button-disable button-event button-radius-1 wpeo-tooltip-event" data-direction="left" aria-label="<?php echo esc_html( $message_tooltip ); ?>"><i class="fas fa-sync"></i></div>
-		<?php
-	endif;
-	?>
-	<div class="statut statut-<?php echo empty( $object->data['external_id'] ) ? 'red' : 'grey'; ?> wpeo-tooltip-event" data-direction="left" aria-label="<?php echo esc_html( $message_tooltip ); ?>"></div>
+<div class="table-cell table-100 wps-sync">
+	<div class="button-synchro <?php echo $can_sync ? 'action-attribute' : 'wpeo-modal-event'; ?>"
+		data-class="synchro-single"
+		<?php // translators: Associate and synchronize object name. ?>
+		data-title="<?php printf( __( 'Associate and synchronize %s', 'wpshop' ), $object->data['title'] ); ?>"
+		data-action="<?php echo $can_sync ? 'sync_entry' : 'load_associate_modal'; ?>"
+		data-type="<?php echo $type; ?>"
+		data-id="<?php echo esc_attr( $object->data['id'] ); ?>"
+		data-nonce="<?php echo esc_attr( wp_create_nonce( $can_sync ? 'sync_entry' : 'load_associate_modal' ) ); ?>"><i class="fas fa-sync"></i></div>
+
+	<div class="statut statut-<?php echo esc_attr( $status_color ); ?> wpeo-tooltip-event" data-direction="left" aria-label="<?php echo esc_html( $message_tooltip ); ?>"></div>
 </div>
